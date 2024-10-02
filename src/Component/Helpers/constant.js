9995e21067115308744cd86103e76970e26a8c3c
malikibrarbhutta@gmail.com <malikibrarbhutta@gmail.com>
2024-10-03 01:26:26 +0500

    --

--- src/Component/Helpers/constant.js
+++ src/Component/Helpers/constant.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://127.0.0.1:8000'
 
-export const BASE_URL= 'https://intellectual-hilary-malik-ibrar-fc32c2c5.koyeb.app'
+export const BASE_URL= 'https://lecturemindbackend-production.up.railway.app'
